4d4eb49e8e0598738401a6f5071bc7f9951a233b
Nicolas <nicolasbonemme@hotmail.com>
2017-09-20 11:56:07 +0200

    Ajout des fonctions de base

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,4 +1,36 @@
 //
 // Created by floryan on 20/09/17.
 //
+#include <stdio.h>
+#include <stdlib.h>
 
+
+
+
+int main(int argc, char * argv[])
+{
+	u_long IpSocket;
+    u_short PortSocket;
+
+
+}
+
+
+
+void login(char *login, char *mdp)
+{
+	//Envoie chaine de caractère au serv avec log + pass et requete LOGIN_OFFICER
+	//Attend réponse
+	//Attend réponse
+}
+
+void logout(char *login)
+{
+	//Envoie chaine de caractère au serv avec log et requete LOGOUT_OFFICER
+}
+
+void check_ticket()
+{
+	//envoie chaine de caractère au serv avec requete CHECK_TICKET
+	//Attend reponse pour encoder bagages
+}
