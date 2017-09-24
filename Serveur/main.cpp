f4acc094023fd3bafb766ee9cea9246279d8f025
Floryan <floryansimar@gmail.com>
2017-09-24 13:46:59 +0200

    Utilisation d'un fichier de log, où sont repris tout les messages affichés dans la console

--- Serveur/main.cpp
+++ Serveur/main.cpp
@@ -13,30 +13,60 @@ int indexClient;
 
 void traitementConnexion(int *num);
 
+void EcrireMessageErrThread(std::string message);
+
+void EcrireMessageOutThread(std::string message);
+
+void EcrireMessageErr(std::string message);
+
+void EcrireMessageOut(std::string message);
+
 std::string getThread();
 
 /*Variables utilisées par les thread pour le pool de thread*/
 Socket *connexion;
 pthread_cond_t condConnexion;
 pthread_mutex_t mutexConnexion;
+pthread_mutex_t mutexLog;
+ofstream log("err.log");
+pthread_key_t keyNumThread;
 
 int main(int argc, char **args) {
     if (argc == 1) {
-        cout << "Trop peu d'arguments à l'execution" << endl;
+        cerr << "Trop peu d'arguments à l'execution" << endl;
+        log << "cerr> Trop peu d'arguments à l'execution" << endl;
         return -1;
     }
     lectureFichierParams(args[1]);
     try {
         if (pthread_cond_init(&condConnexion, nullptr) == -1) {
-            cerr << "Impossible d'initialiser le pthread_cond: " << strerror(errno) << endl;
+            cerr << "Impossible d'initialiser le condition condConnexion: " << strerror(errno) << endl;
+            log << "cerr> Impossible d'initialiser la condition condConnexion: " << strerror(errno) << endl;
             return -1;
         }
         if (pthread_mutex_init(&mutexConnexion, nullptr) == -1) {
-            cerr << "Impossible d'initialiser le mutex: " << strerror(errno) << endl;
+            cerr << "Impossible d'initialiser le mutex mutexConnexion: " << strerror(errno) << endl;
+            log << "cerr> Impossible d'initialiser le mutex mutexConnexion: " << strerror(errno) << endl;
             return -2;
         }
-        pthread_t pthread[nbThread];
+        if (pthread_mutex_init(&mutexLog, nullptr) == -1) {
+            cerr << "Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
+            log << "cerr> Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
+            return -3;
+        }
+        if (pthread_key_create(&keyNumThread, nullptr) == -1) {
+            cerr << "Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
+            log << "cerr> Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
+            return -4;
+        }
+        // Création du socket
+        // Any = 0.0.0.0 => Ecoute toute les addresse
+        // Parametres.PortRange[0] => Premier port disponible pour le serveur
+        SocketServeur socket(ipv4().Any, Parametres.PortRange[0]);
+        cout << "(Socket principal)> " << socket.getIp() << ":" << socket.getPort() << endl;
+        log << "cout> (Socket principal)> " << socket.getIp() << ":" << socket.getPort() << endl;
 
+        pthread_t pthread[nbThread];
         for (int i = 0; i < nbThread; i++) {
             //Passage d'un pointeur pour que la valeur ne soit pas modifiée
             int *param = new int;
@@ -45,8 +75,6 @@ int main(int argc, char **args) {
             pthread_detach(pthread[i]);
         }
 
-        SocketServeur socket(ipv4().Any, Parametres.PortRange[0]);
-        cout << "(Socket principal)> " << socket.getIp() << ":" << socket.getPort() << endl;
         while (1) {
             try {
                 socket.Listen();
@@ -67,19 +95,52 @@ int main(int argc, char **args) {
     return 0;
 }
 
-std::string getThread(int num) {
+std::string getThread() {
+    int num = *((int *) pthread_getspecific(keyNumThread));
     std::string retour;
     retour += "th_" + std::to_string(num) + "> ";
     return retour;
 }
 
+void EcrireMessageErrThread(std::string message) {
+    cerr << getThread() << message << endl;
+    pthread_mutex_lock(&mutexLog);
+    log << "cerr> " << message << endl;
+    pthread_mutex_unlock(&mutexLog);
+}
+
+void EcrireMessageOutThread(std::string message) {
+    cerr << getThread() << message << endl;
+    pthread_mutex_lock(&mutexLog);
+    log << "cout> " << message << endl;
+    pthread_mutex_unlock(&mutexLog);
+}
+
+void EcrireMessageErr(std::string message) {
+    cerr << message << endl;
+    pthread_mutex_lock(&mutexLog);
+    log << "cerr> " << message << endl;
+    pthread_mutex_unlock(&mutexLog);
+}
+
+void EcrireMessageOut(std::string message) {
+    cout << message << endl;
+    pthread_mutex_lock(&mutexLog);
+    log << "cout> " << message << endl;
+    pthread_mutex_unlock(&mutexLog);
+}
+
 void traitementConnexion(int *num) {
-    int numero = *num;
-    //Suppression de l'allocation dynamique
-    delete num;
+    if (pthread_setspecific(keyNumThread, num) != 0) {
+        EcrireMessageErr(
+                std::string("th_") + std::to_string(*num) + "> Impossible de mettre num(" + std::to_string(*num) +
+                ") dans la variable spécifique au thread.");
+        EcrireMessageErr(std::string("th_") + std::to_string(*num) + "> Fin de l'execution de ce thread.");
+        return;
+    }
 
     Socket *s = nullptr;
-    cout << getThread(numero) << "Démarrage" << endl;
+
     while (1) {
         pthread_mutex_lock(&mutexConnexion);
         while (s == nullptr) {
@@ -87,7 +148,7 @@ void traitementConnexion(int *num) {
             s = connexion;
         }
         pthread_mutex_unlock(&mutexConnexion);
-        cout << getThread(numero) << s->toString() << " est connecté." << endl;
+        EcrireMessageOutThread(s->toString() + " est connecté.");
         bool stop = false;
         while (!stop) {
             try {
@@ -113,7 +174,7 @@ void traitementConnexion(int *num) {
                         break;
                         //Unique point de sortie d'un socket passif du serveur
                     case DISCONNECT: {
-                        cout << getThread(numero) << s->toString() << " s'est déconnecté." << endl;
+                        EcrireMessageOutThread(s->toString() + " s'est déconnecté.");
                         delete s;
                         s = nullptr;
                         clients--;
@@ -121,13 +182,16 @@ void traitementConnexion(int *num) {
                     }
                         break;
                     default:
-                        cout << getThread(numero) << "Message <" << sMessage.message << "> de type <" << sMessage.type
-                             << "> reçu de "
-                             << s->toString() << endl;
+                        EcrireMessageOutThread(std::string("Message <")
+                                               + sMessage.message
+                                               + "> de type <" +
+                                               std::to_string(sMessage.type)
+                                               + "> reçu de "
+                                               + s->toString());
                         break;
                 }
             } catch (Exception e) {
-                cerr << getThread(numero) << e.getMessage() << endl;
+                EcrireMessageErrThread(e.getMessage());
             }
         }
     }
