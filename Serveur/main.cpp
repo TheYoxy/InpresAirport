ea3dddb56c92210ff05ccc0880b8ff6ff0832c24
Floryan <floryansimar@gmail.com>
2017-09-24 16:12:06 +0200

    Mise en place d'une finition propre du serveur via le signal SIGINT

--- Serveur/main.cpp
+++ Serveur/main.cpp
@@ -1,4 +1,5 @@
 #include <netdb.h>
+#include <signal.h>
 #include "../Librairie/SocketClient.h"
 #include "../Librairie/SocketServeur.h"
 //FLUX ERREUR: FICHIER LOG
@@ -8,8 +9,6 @@ using namespace std;
 extern SParametres Parametres;
 extern int maxSocketNbr;
 extern int clients;
-
-int indexClient;
 
 void traitementConnexion(int *num);
 
@@ -27,9 +26,18 @@ std::string getThread();
 Socket *connexion;
 pthread_cond_t condConnexion;
 pthread_mutex_t mutexConnexion;
+/* Variables utilisées pour le logging */
 pthread_mutex_t mutexLog;
 ofstream log("err.log");
 pthread_key_t keyNumThread;
+/* Variables globales pour couper le serveur */
+pthread_t pthread[nbThread];
+// Création du socket
+// Any = 0.0.0.0 => Ecoute toute les addresse
+// Parametres.PortRange[0] => Premier port disponible pour le serveur
+SocketServeur *socketPrincipal = nullptr;
+
+void HandlerSignal(int sig);
 
 int main(int argc, char **args) {
     if (argc == 1) {
@@ -38,35 +46,39 @@ int main(int argc, char **args) {
         return -1;
     }
     lectureFichierParams(args[1]);
+    cout << EBADF << endl;
     try {
+        socketPrincipal = new SocketServeur(ipv4().Any, Parametres.PortRange[0]);
+        cout << "(Socket principal)> " << socketPrincipal->getIp() << ":" << socketPrincipal->getPort() << endl;
+        log << "cout> (Socket principal)> " << socketPrincipal->getIp() << ":" << socketPrincipal->getPort() << endl;
         if (pthread_cond_init(&condConnexion, nullptr) == -1) {
             cerr << "Impossible d'initialiser le condition condConnexion: " << strerror(errno) << endl;
             log << "cerr> Impossible d'initialiser la condition condConnexion: " << strerror(errno) << endl;
-            return -1;
+            return -2;
         }
         if (pthread_mutex_init(&mutexConnexion, nullptr) == -1) {
             cerr << "Impossible d'initialiser le mutex mutexConnexion: " << strerror(errno) << endl;
             log << "cerr> Impossible d'initialiser le mutex mutexConnexion: " << strerror(errno) << endl;
-            return -2;
+            return -3;
         }
         if (pthread_mutex_init(&mutexLog, nullptr) == -1) {
             cerr << "Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
             log << "cerr> Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
-            return -3;
+            return -4;
         }
         if (pthread_key_create(&keyNumThread, nullptr) == -1) {
             cerr << "Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
             log << "cerr> Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
-            return -4;
+            return -5;
         }
-        // Création du socket
-        // Any = 0.0.0.0 => Ecoute toute les addresse
-        // Parametres.PortRange[0] => Premier port disponible pour le serveur
-        SocketServeur socket(ipv4().Any, Parametres.PortRange[0]);
-        cout << "(Socket principal)> " << socket.getIp() << ":" << socket.getPort() << endl;
-        log << "cout> (Socket principal)> " << socket.getIp() << ":" << socket.getPort() << endl;
-
-        pthread_t pthread[nbThread];
+        struct sigaction sig;
+        sigemptyset(&sig.sa_mask);
+        sig.sa_handler = HandlerSignal;
+        if (sigaction(SIGINT, &sig, nullptr) == -1) {
+            cerr << "Impossible d'armer le signal SIGKILL: " << strerror(errno) << endl;
+            log << "cerr> Impossible d'armer le signal SIGKILL: " << strerror(errno) << endl;
+            return -6;
+        }
         for (int i = 0; i < nbThread; i++) {
             //Passage d'un pointeur pour que la valeur ne soit pas modifiée
             int *param = new int;
@@ -75,24 +87,40 @@ int main(int argc, char **args) {
             pthread_detach(pthread[i]);
         }
 
-        while (1) {
+        while (socketPrincipal != nullptr) {
             try {
-                socket.Listen();
-                Socket *s = socket.Accept();
+                Socket *s = socketPrincipal->Accept();
                 pthread_mutex_lock(&mutexConnexion);
                 connexion = s;
                 pthread_mutex_unlock(&mutexConnexion);
                 pthread_cond_signal(&condConnexion);
             }
             catch (Exception e) {
-                cerr << e.getMessage() << endl;
+                if (errno != EINTR)
+                    EcrireMessageErr(e.getMessage());
+                else
+                    EcrireMessageErr("Fin de l'execution du serveur");
             }
         }
     }
     catch (Exception e) {
-        cerr << e.getMessage() << endl;
+        EcrireMessageErr(e.getMessage());
     }
+    log.close();
     return 0;
+}
+
+
+void HandlerSignal(int sig) {
+    cout << "SIGINT reçu" << endl;
+    cout << "Début du handler de supression" << endl;
+    for (int i = 0; i < nbThread; i++) {
+        if (pthread_cancel(pthread[i]) != 0) {
+            EcrireMessageErr("Impossible de cancel le thread numero[" + to_string(i) + "]");
+        }
+    }
+    delete socketPrincipal;
+    socketPrincipal = nullptr;
 }
 
 std::string getThread() {
@@ -131,6 +159,10 @@ void EcrireMessageOut(std::string message) {
 }
 
 void traitementConnexion(int *num) {
+    // Initialisation des threads
+    sigset_t mask;
+    Socket *s = nullptr;
+
     if (pthread_setspecific(keyNumThread, num) != 0) {
         EcrireMessageErr(
                 std::string("th_") + std::to_string(*num) + "> Impossible de mettre num(" + std::to_string(*num) +
@@ -138,9 +170,32 @@ void traitementConnexion(int *num) {
         EcrireMessageErr(std::string("th_") + std::to_string(*num) + "> Fin de l'execution de ce thread.");
         return;
     }
-
-    Socket *s = nullptr;
-
+    if (sigemptyset(&mask) != 0) {
+        EcrireMessageErrThread("Impossible de réinitialiser le mask");
+        EcrireMessageErrThread("Fin de l'execution de ce thread");
+        return;
+    }
+    if (sigaddset(&mask, SIGINT) != 0) {
+        EcrireMessageErrThread("Impossible d'ajouter SIGKILL au mask");
+        EcrireMessageErrThread("Fin de l'execution de ce thread");
+        return;
+    }
+    if (sigprocmask(SIG_SETMASK, &mask, nullptr) != 0) {
+        EcrireMessageErrThread("Impossible de mettre en place le mask");
+        EcrireMessageErrThread("Fin de l'execution de ce thread");
+        return;
+    }
+    if (pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, nullptr) != 0) {
+        EcrireMessageErrThread("Impossible de mettre en place le cancelstate");
+        EcrireMessageErrThread("Fin de l'execution de ce thread");
+        return;
+    }
+    if (pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, nullptr) != 0) {
+        EcrireMessageErrThread("Impossible de mettre en place le canceltype");
+        EcrireMessageErrThread("Fin de l'execution de ce thread");
+        return;
+    }
+    // Fin initialisation du thread
     while (1) {
         pthread_mutex_lock(&mutexConnexion);
         while (s == nullptr) {
