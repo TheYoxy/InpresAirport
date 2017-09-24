116d8d2e239e10cd8ff4f66d7013ad87526730f2
Floryan <floryansimar@gmail.com>
2017-09-24 19:29:00 +0200

    Modification du passage de paramètres de string, pour passer les objets par référence, et non plus faire une copie des objets

--- Serveur/main.cpp
+++ Serveur/main.cpp
@@ -12,16 +12,17 @@ extern int clients;
 
 void traitementConnexion(int *num);
 
-void EcrireMessageErrThread(std::string message);
+void EcrireMessageErrThread(const std::string &message);
 
-void EcrireMessageOutThread(std::string message);
+void EcrireMessageOutThread(const std::string &message);
 
-void EcrireMessageErr(std::string message);
+void EcrireMessageErr(const std::string &message);
 
-void EcrireMessageOut(std::string message);
+void EcrireMessageOut(const std::string &message);
 
 std::string getThread();
 
+bool userExist(const std::string &user, const std::string &password);
 /*Variables utilisées par les thread pour le pool de thread*/
 Socket *connexion;
 pthread_cond_t condConnexion;
@@ -30,11 +31,8 @@ pthread_mutex_t mutexConnexion;
 pthread_mutex_t mutexLog;
 ofstream log("err.log");
 pthread_key_t keyNumThread;
-pthread_key_t keySocket;
 /* Variables globales pour couper le serveur */
 pthread_t pthread[nbThread];
-pthread_mutex_t mutexTMain;
-pthread_t tmain;
 // Création du socket
 // Any = 0.0.0.0 => Ecoute toute les addresse
 // Parametres.PortRange[0] => Premier port disponible pour le serveur
@@ -42,16 +40,16 @@ SocketServeur *socketPrincipal = nullptr;
 
 void HandlerSignal(int sig);
 
-void cleanupThread(void *sig);
-
 int main(int argc, char **args) {
-    cout << "\x1B[2J\x1B[H";
     if (argc == 1) {
         cerr << "Trop peu d'arguments à l'execution" << endl;
         log << "cerr> Trop peu d'arguments à l'execution" << endl;
         return -1;
     }
     lectureFichierParams(args[1]);
+    std::string a, b;
+    userExist(a, b);
+    return 0;
     try {
         socketPrincipal = new SocketServeur(ipv4().Any, Parametres.PortRange[0]);
         cout << "(Socket principal)> " << socketPrincipal->getIp() << ":" << socketPrincipal->getPort() << endl;
@@ -71,20 +69,10 @@ int main(int argc, char **args) {
             log << "cerr> Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
             return -4;
         }
-        if (pthread_mutex_init(&mutexTMain, nullptr) == -1) {
-            cerr << "Impossible d'initialiser le mutex mutexTMain: " << strerror(errno) << endl;
-            log << "cerr> Impossible d'initialiser le mutex mutexTMain: " << strerror(errno) << endl;
-            return -5;
-        }
         if (pthread_key_create(&keyNumThread, nullptr) == -1) {
             cerr << "Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
             log << "cerr> Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
-            return -6;
-        }
-        if (pthread_key_create(&keySocket, nullptr) == -1) {
-            cerr << "Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
-            log << "cerr> Impossible d'initialiser le mutex mutexLog: " << strerror(errno) << endl;
-            return -7;
+            return -5;
         }
         struct sigaction sig;
         sigemptyset(&sig.sa_mask);
@@ -92,9 +80,8 @@ int main(int argc, char **args) {
         if (sigaction(SIGINT, &sig, nullptr) == -1) {
             cerr << "Impossible d'armer le signal SIGKILL: " << strerror(errno) << endl;
             log << "cerr> Impossible d'armer le signal SIGKILL: " << strerror(errno) << endl;
-            return -8;
+            return -6;
         }
-        tmain = pthread_self();
         for (int i = 0; i < nbThread; i++) {
             //Passage d'un pointeur pour que la valeur ne soit pas modifiée
             int *param = new int;
@@ -103,7 +90,6 @@ int main(int argc, char **args) {
             pthread_detach(pthread[i]);
         }
 
-        int i = 00;
         while (socketPrincipal != nullptr) {
             try {
                 Socket *s = socketPrincipal->Accept();
@@ -127,20 +113,14 @@ int main(int argc, char **args) {
     return 0;
 }
 
+
 void HandlerSignal(int sig) {
-    //Permet de retourner au début de la ligne
-    printf("\r");
     cout << "SIGINT reçu" << endl;
     cout << "Début du handler de supression" << endl;
     for (int i = 0; i < nbThread; i++) {
-        cout << "Envoi du signal au thread " << i << endl;
-        if (pthread_cancel(pthread[i]) != 0) //TODO Demander si le cancel fait sortir d'une fonction en attente
-        {
+        if (pthread_cancel(pthread[i]) != 0) {
             EcrireMessageErr("Impossible de cancel le thread numero[" + to_string(i) + "]");
         }
-        pthread_yield();
-        sched_yield();
-//        pthread_join(pthread[i], nullptr);
     }
     delete socketPrincipal;
     socketPrincipal = nullptr;
@@ -153,55 +133,52 @@ std::string getThread() {
     return retour;
 }
 
-void EcrireMessageErrThread(std::string message) {
-    pthread_mutex_lock(&mutexLog);
+void EcrireMessageErrThread(const std::string &message) {
     cerr << getThread() << message << endl;
+    pthread_mutex_lock(&mutexLog);
     log << "cerr> " << message << endl;
     pthread_mutex_unlock(&mutexLog);
 }
 
-void EcrireMessageOutThread(std::string message) {
-    pthread_mutex_lock(&mutexLog);
+void EcrireMessageOutThread(const std::string &message) {
     cerr << getThread() << message << endl;
+    pthread_mutex_lock(&mutexLog);
     log << "cout> " << message << endl;
     pthread_mutex_unlock(&mutexLog);
 }
 
-void EcrireMessageErr(std::string message) {
-    pthread_mutex_lock(&mutexLog);
+void EcrireMessageErr(const std::string &message) {
     cerr << message << endl;
+    pthread_mutex_lock(&mutexLog);
     log << "cerr> " << message << endl;
     pthread_mutex_unlock(&mutexLog);
 }
 
-void EcrireMessageOut(std::string message) {
-    pthread_mutex_lock(&mutexLog);
+void EcrireMessageOut(const std::string &message) {
     cout << message << endl;
+    pthread_mutex_lock(&mutexLog);
     log << "cout> " << message << endl;
     pthread_mutex_unlock(&mutexLog);
 }
 
-void cleanupThread(void *sig) {
-    EcrireMessageErrThread("Fonction cleanup");
-    Socket *s = (Socket *) pthread_getspecific(keySocket);
-    if (s != nullptr) {
-        s->Close();
-        delete s;
-    }
-    pthread_setspecific(keySocket, nullptr);
-    EcrireMessageErr("Socket supprimé");
-    int *i = (int *) pthread_getspecific(keyNumThread);
-    if (s != nullptr)
-        delete i;
-    pthread_setspecific(keyNumThread, nullptr);
-    cout << "Fin du cleanup" << endl;
+bool userExist(const std::string &user, const std::string &password) {
+    std::string message;
+    ifstream userFile(Parametres.userDB);
+    userFile >> message;
+    cout << "Lu: " << message << endl;
 }
-
 void traitementConnexion(int *num) {
     // Initialisation des threads
     sigset_t mask;
     Socket *s = nullptr;
-    pthread_setspecific(keyNumThread, num);
+
+    if (pthread_setspecific(keyNumThread, num) != 0) {
+        EcrireMessageErr(
+                std::string("th_") + std::to_string(*num) + "> Impossible de mettre num(" + std::to_string(*num) +
+                ") dans la variable spécifique au thread.");
+        EcrireMessageErr(std::string("th_") + std::to_string(*num) + "> Fin de l'execution de ce thread.");
+        return;
+    }
     if (sigemptyset(&mask) != 0) {
         EcrireMessageErrThread("Impossible de réinitialiser le mask");
         EcrireMessageErrThread("Fin de l'execution de ce thread");
@@ -228,66 +205,59 @@ void traitementConnexion(int *num) {
         return;
     }
     // Fin initialisation du thread
-    pthread_cleanup_push(cleanupThread, 0);
-        bool boucle = true;
-        while (boucle) {
-            pthread_mutex_lock(&mutexConnexion);
-            while (s == nullptr) {
-                pthread_cond_wait(&condConnexion, &mutexConnexion);
-                s = connexion;
-                pthread_setspecific(keySocket, s);
-            }
-            pthread_mutex_unlock(&mutexConnexion);
-            EcrireMessageOutThread(s->toString() + " est connecté.");
-            bool stop = false;
-            while (!stop) {
-                try {
-                    std::string message;
-                    s->Recv(message);
-                    s->SendAck();
-                    SMessage sMessage = getStructMessageFromString(message);
-                    switch (sMessage.type) {
-                        case LOGIN_OFFICER:
-
-                            break;
-                        case LOGOUT_OFFICER:
-
-                            break;
-                        case CHECK_TICKET:
-
-                            break;
-                        case CHECK_LUGGAGE:
-
-                            break;
-                        case PAYMENT_DONE:
-
-                            break;
-                            //Unique point de sortie d'un socket passif du serveur
-                        case DISCONNECT: {
-                            EcrireMessageOutThread(s->toString() + " s'est déconnecté.");
-                            delete s;
-                            s = nullptr;
-                            clients--;
-                            stop = true;
-                        }
-                            break;
-                        default:
-                            EcrireMessageOutThread(std::string("Message <")
-                                                   + sMessage.message
-                                                   + "> de type <" +
-                                                   std::to_string(sMessage.type)
-                                                   + "> reçu de "
-                                                   + s->toString());
-                            break;
-                    }
-                } catch (Exception e) {
-                    if (errno == EINTR)
-                        boucle = false;
-                    EcrireMessageErrThread(e.getMessage());
-                }
-            }
-            pthread_setspecific(keySocket, nullptr);
+    while (1) {
+        pthread_mutex_lock(&mutexConnexion);
+        while (s == nullptr) {
+            pthread_cond_wait(&condConnexion, &mutexConnexion);
+            s = connexion;
         }
-    pthread_cleanup_pop(1);
-    return;
+        pthread_mutex_unlock(&mutexConnexion);
+        EcrireMessageOutThread(s->toString() + " est connecté.");
+        bool stop = false;
+        while (!stop) {
+            try {
+                std::string message;
+                s->Recv(message);
+                s->SendAck();
+                SMessage sMessage = getStructMessageFromString(message);
+                switch (sMessage.type) {
+                    case LOGIN_OFFICER:
+
+                        break;
+                    case LOGOUT_OFFICER:
+
+                        break;
+                    case CHECK_TICKET:
+
+                        break;
+                    case CHECK_LUGGAGE:
+
+                        break;
+                    case PAYMENT_DONE:
+
+                        break;
+                        //Unique point de sortie d'un socket passif du serveur
+                    case DISCONNECT: {
+                        EcrireMessageOutThread(s->toString() + " s'est déconnecté.");
+                        s->Close();
+                        delete s;
+                        s = nullptr;
+                        clients--;
+                        stop = true;
+                    }
+                        break;
+                    default:
+                        EcrireMessageOutThread(std::string("Message <")
+                                               + sMessage.message
+                                               + "> de type <" +
+                                               std::to_string(sMessage.type)
+                                               + "> reçu de "
+                                               + s->toString());
+                        break;
+                }
+            } catch (Exception e) {
+                EcrireMessageErrThread(e.getMessage());
+            }
+        }
+    }
 }
