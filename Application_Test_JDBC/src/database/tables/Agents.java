76780aa93e2e2bde4110c042bdbb10008644732c
Floryan Simar <floryansimar@gmail.com>
2017-10-10 17:28:41 +0200

    Ajout setters

--- Application_Test_JDBC/src/database/tables/Agents.java
+++ Application_Test_JDBC/src/database/tables/Agents.java
@@ -22,11 +22,23 @@ public class Agents {
         return this.Nom;
     }
 
+    public void setNom(String nom) {
+        Nom = nom;
+    }
+
     public String getPrenom() {
         return this.Prenom;
     }
 
+    public void setPrenom(String prenom) {
+        Prenom = prenom;
+    }
+
     public String getPoste() {
         return this.Poste;
+    }
+
+    public void setPoste(String poste) {
+        Poste = poste;
     }
 }
