b064eafc5aeb39583367ff9bd9c1b7aa701016ed
Floryan Simar <floryansimar@gmail.com>
2017-10-10 12:06:44 +0200

    Correction types

--- SQL/Oracle/tables.sql
+++ SQL/Oracle/tables.sql
@@ -1,18 +1,18 @@
 DROP TABLE activites;
 CREATE TABLE activites (
   id          NUMBER(6),
-  cours       CHAR(50),
-  type        CHAR(30),
+  cours       VARCHAR2(50),
+  type        VARCHAR2(30),
   datep       DATE,
-  description CHAR(50),
-  reference   CHAR(25),
+  description VARCHAR2(50),
+  reference   VARCHAR2(25),
   CONSTRAINT act$pk PRIMARY KEY (id)
 );
 DROP TABLE intervenant CASCADE CONSTRAINTS;
 CREATE TABLE intervenant (
   id     NUMBER(6),
-  nom    CHAR(20),
-  prenom CHAR(20),
-  statut CHAR(30),
+  nom    VARCHAR2(20),
+  prenom VARCHAR2(20),
+  statut VARCHAR2(30),
   CONSTRAINT inter$pk PRIMARY KEY (id)
 );
