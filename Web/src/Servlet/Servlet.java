0a3b7ce10573c76c6833548f5a1cf36692b407a3
Floryan <floryansimar@gmail.com>
2017-11-08 13:45:44 +0100

    Création servlet

---
+++ Web/src/Servlet/Servlet.java
@@ -0,0 +1,17 @@
+package Servlet;
+
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        HttpSession hs = request.getSession();
+        PrintWriter pw = response.getWriter();
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
